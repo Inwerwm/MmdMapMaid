3dbc2974b0f641275ff380ab5bc2d9e892754d2f
Inwerwm <saclainvervm@gmail.com>
2022-09-11 17:15:30 +0900

    PMMパス書き換えの操作UIを実装

---
+++ MmdMapMaid/Helpers/StorageHelper.cs
@@ -0,0 +1,24 @@
+﻿using Windows.Storage.Pickers;
+using Windows.Storage;
+
+namespace MmdMapMaid.Helpers;
+
+class StorageHelper
+{
+    public static async Task<StorageFile> PickSingleFileAsync(params string[] fileTypes) =>
+    await CreateOpenFilePicker(fileTypes).PickSingleFileAsync();
+
+    public static async Task<IEnumerable<StorageFile>> PickMultipleFilesAsync(params string[] fileTypes) =>
+        await CreateOpenFilePicker(fileTypes).PickMultipleFilesAsync();
+
+    private static FileOpenPicker CreateOpenFilePicker(params string[] fileTypes)
+    {
+        var openPicker = new FileOpenPicker();
+        foreach (var type in fileTypes)
+        {
+            openPicker.FileTypeFilter.Add(type);
+        }
+        WinRT.Interop.InitializeWithWindow.Initialize(openPicker, WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow));
+        return openPicker;
+    }
+}
--- MmdMapMaid/ViewModels/PmmViewModel.cs
+++ MmdMapMaid/ViewModels/PmmViewModel.cs
@@ -1,5 +1,10 @@
 ﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.WinUI.Helpers;
+using Microsoft.UI.Xaml.Controls;
+using MmdMapMaid.Core.Models.Pmm;
+using MmdMapMaid.Helpers;
 using MmdMapMaid.Observables;
 
 namespace MmdMapMaid.ViewModels;
@@ -9,8 +14,68 @@ public partial class PmmViewModel : ObservableRecipient
     [ObservableProperty]
     private ObservableCollection<PmmModelInformation> _modelInfo;
 
+    [ObservableProperty]
+    private InfoBarSeverity _writePmmInfoSeverty;
+    [ObservableProperty]
+    private bool _openCompleteMessage;
+    [ObservableProperty]
+    private string _writingMessage;
+
+    private ModelReplacer? Replacer
+    {
+        get;
+        set;
+    }
+
     public PmmViewModel()
     {
         _modelInfo = new();
+        _openCompleteMessage = false;
+    }
+
+    private void NoticeStartWrite()
+    {
+        WritePmmInfoSeverty = InfoBarSeverity.Informational;
+        WritingMessage = "Writing...";
+        OpenCompleteMessage = true;
+    }
+
+    private void NoticeEndWrite()
+    {
+        WritePmmInfoSeverty = InfoBarSeverity.Success;
+        WritingMessage = "Pmm file has been written.";
+        OpenCompleteMessage = true;
+    }
+
+    [ICommand]
+    private async Task ReadPmm()
+    {
+        var file = await StorageHelper.PickSingleFileAsync(".pmm");
+        if (file == null) { return; }
+
+        Replacer = new(file.Path);
+
+        ModelInfo.Clear();
+        foreach (var (name, path, index) in Replacer.GetModelList())
+        {
+            ModelInfo.Add(new(index, name, path));
+        }
+    }
+
+    [ICommand]
+    private void WritePmm()
+    {
+        if (Replacer is null) { return; }
+
+        NoticeStartWrite();
+
+        foreach (var item in ModelInfo)
+        {
+            Replacer.Replace(item.Index, item.Path);
+        }
+
+        Replacer.Save();
+
+        NoticeEndWrite();
     }
 }
