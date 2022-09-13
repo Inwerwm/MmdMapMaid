e645f5d74faaa29951aa3cfab45360f9eb1cf939
Inwerwm <saclainvervm@gmail.com>
2022-09-13 21:19:35 +0900

    EMMをプロパティとして持つ設計に変更

--- MmdMapMaid.Core/Models/Emm/EmmOrderMapper.cs
+++ MmdMapMaid.Core/Models/Emm/EmmOrderMapper.cs
@@ -5,18 +5,17 @@ using MikuMikuMethods.Pmx;
 namespace MmdMapMaid.Core.Models.Emm;
 public class EmmOrderMapper
 {
-    public static IEnumerable<string> GetObjectPaths(string emmPath) => new EmmData(emmPath).Objects.Select(obj => obj.Path);
-
-    public SaveOptions Options
+    public EmmData Emm
     {
         get;
-        init;
     }
 
-    public EmmOrderMapper(SaveOptions? options = null)
+    public EmmOrderMapper(string emmPath)
     {
-        Options ??= options ?? new();
+        Emm = new(emmPath);
     }
+
+    public IEnumerable<string> GetObjectPaths() => Emm.Objects.Select(obj => obj.Path);
 
     /// <summary>
     /// EMMファイルの材質順をモデルに合わせて並び替える
@@ -25,28 +24,29 @@ public class EmmOrderMapper
     /// <param name="destinationModelPath">並替先材質順のモデル</param>
     /// <param name="targetEmmPath">変換対象EMMファイル</param>
     /// <returns>新規作成ファイルのパス(上書き時はバックアップ、そうでなければ並び替え後ファイル)</returns>
-    public string? Run(string sourceModelPath, string destinationModelPath, string targetEmmPath)
+    public string? Run(string sourceModelPath, string destinationModelPath, string targetEmmPath, SaveOptions? options = null)
     {
-        var mappedEmm = MapOrder(Path.GetFullPath(sourceModelPath), Path.GetFullPath(destinationModelPath), targetEmmPath);
-        return Options.SaveWithBackupAndReturnCreatedPath(targetEmmPath, savePath => mappedEmm.Write(savePath));
+        MapOrder(Path.GetFullPath(sourceModelPath), Path.GetFullPath(destinationModelPath), targetEmmPath);
+
+        options ??= new SaveOptions();
+        return options.SaveWithBackupAndReturnCreatedPath(targetEmmPath, savePath => Emm.Write(savePath));
     }
 
-    private static EmmData MapOrder(string sourceModelPath, string destinationModelPath, string targetEmmPath)
+    private void MapOrder(string sourceModelPath, string destinationModelPath, string targetEmmPath)
     {
         var sourceModel = new PmxModel(sourceModelPath);
         var destinationModel = new PmxModel(destinationModelPath);
 
         var indexMap = sourceModel.Materials.Select(srcMat => destinationModel.Materials.FindIndex(destMat => srcMat.Name == destMat.Name)).ToArray();
 
-        var emm = new EmmData(targetEmmPath);
-        var targetObjects = emm.Objects.FindAll(obj => obj.Path == sourceModelPath);
+        var targetObjects = Emm.Objects.FindAll(obj => obj.Path == sourceModelPath);
 
         foreach (var targetObj in targetObjects)
         {
             targetObj.Path = destinationModelPath;
         }
 
-        foreach (var objSetting in emm.EffectSettings.SelectMany(es => es.ObjectSettings).Where(os => targetObjects.Contains(os.Object)))
+        foreach (var objSetting in Emm.EffectSettings.SelectMany(es => es.ObjectSettings).Where(os => targetObjects.Contains(os.Object)))
         {
             var mappedSubset = new EmmMaterial[destinationModel.Materials.Count];
 
@@ -64,7 +64,5 @@ public class EmmOrderMapper
                 objSetting.Subsets.Add(subset ?? new());
             }
         }
-
-        return emm;
     }
 }
