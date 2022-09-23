0548e672fe9d78c042139a24f14e9dbe530c00cd
Inwerwm <saclainvervm@gmail.com>
2022-09-23 23:14:19 +0900

    アイコンを変更

--- MmdMapMaid/MainWindow.xaml.cs
+++ MmdMapMaid/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ public sealed partial class MainWindow : WindowEx
     {
         InitializeComponent();
 
-        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/maid.ico"));
         Content = null;
         Title = "AppDisplayName".GetLocalized();
     }
