4e04b2fb749ad9f9d43531dfcf7b9e05426b8004
Inwerwm <saclainvervm@gmail.com>
2022-09-23 22:17:34 +0900

    PMMファイルのD&Dが可能なことを示す表示を追加

---
+++ MmdMapMaid/Helpers/NegatedBoolToVisibility.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.WinUI.UI.Converters;
+using Microsoft.UI.Xaml.Data;
+
+namespace MmdMapMaid.Helpers;
+
+internal class NegatedBoolToVisibility : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language) =>
+        new BoolToVisibilityConverter().Convert(value is bool b && !b, targetType, parameter, language);
+    public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+        new BoolToVisibilityConverter().Convert(value is bool b && !b, targetType, parameter, language);
+}
