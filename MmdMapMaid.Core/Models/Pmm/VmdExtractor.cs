b8168878c6046fd4349c669ee4060196f473b704
Inwerwm <saclainvervm@gmail.com>
2022-10-01 20:16:37 +0900

    VMD抽出処理の呼び出しを実装

---
+++ MmdMapMaid.Core/Models/Pmm/VmdExtractor.cs
@@ -0,0 +1,35 @@
+﻿using MikuMikuMethods.Converter;
+using MikuMikuMethods.Pmm;
+
+namespace MmdMapMaid.Core.Models.Pmm;
+public class VmdExtractor
+{
+    public PolygonMovieMaker Pmm
+    {
+        get;
+    }
+
+    private string PmmPath
+    {
+        get;
+    }
+
+    public string[] ModelNames => Pmm.Models.Select(m => m.Name).ToArray();
+
+    public VmdExtractor(string pmmPath)
+    {
+        PmmPath = pmmPath;
+        Pmm = new(PmmPath);
+    }
+
+    public void ExtractModelMotion(int modelIndex, string saveDirectory, ModelMotionExtractionOptions? options = null)
+    {
+        var targetModel = Pmm.Models[modelIndex];
+        targetModel.ExtractModelMotion(options).Write(Path.Combine(saveDirectory, $"{Path.GetFileNameWithoutExtension(PmmPath)}_{targetModel.Name}.vmd"));
+    }
+
+    public void ExtractCameraMotion(string saveDirectory, CameraMotionExtractionOptions? options)
+    {
+        Pmm.ExtractCameraMotion(options).Write(Path.Combine(saveDirectory, $"{Path.GetFileNameWithoutExtension(PmmPath)}_Camera.vmd"));
+    }
+}
