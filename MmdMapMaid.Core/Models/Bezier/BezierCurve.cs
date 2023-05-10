6f877482f579cdff3ad36e68e3fc8b696c29ed4b
Inwerwm <saclainvervm@gmail.com>
2023-05-10 21:12:44 +0900

    ベジェ曲線を計算するクラスを追加

---
+++ MmdMapMaid.Core/Models/Bezier/BezierCurve.cs
@@ -0,0 +1,40 @@
+﻿using MikuMikuMethods.Common;
+
+namespace MmdMapMaid.Core.Models.Bezier;
+public class BezierCurve
+{
+    public static Point2<double> CalculateCubicBezierPoint(double t, Point2<double> p0, Point2<double> p1, Point2<double> p2, Point2<double> p3)
+    {
+        var u = 1 - t;
+        var tt = t * t;
+        var uu = u * u;
+        var uuu = uu * u;
+        var ttt = tt * t;
+
+        var point = new Point2<double>(uuu * p0.X + 3 * uu * t * p1.X + 3 * u * tt * p2.X + ttt * p3.X,
+                              uuu * p0.Y + 3 * uu * t * p1.Y + 3 * u * tt * p2.Y + ttt * p3.Y);
+
+        return point;
+    }
+
+    public static List<Point2<double>> SampleCubicBezierCurve(Point2<double> p0, Point2<double> p1, Point2<double> p2, Point2<double> p3, double xStep, double precision = 0.01)
+    {
+        var sampledPoints = new List<Point2<double>>();
+        double t = 0;
+        var currentX = p0.X;
+
+        while (t <= 1.0)
+        {
+            var point = CalculateCubicBezierPoint(t, p0, p1, p2, p3);
+            if (System.Math.Abs(point.X - currentX) >= xStep)
+            {
+                sampledPoints.Add(point);
+                currentX = point.X;
+            }
+
+            t += precision;
+        }
+
+        return sampledPoints;
+    }
+}
---
+++ MmdMapMaid.Core/Models/Bezier/SimplifyPath.cs
@@ -0,0 +1,57 @@
+﻿using MikuMikuMethods.Common;
+
+namespace MmdMapMaid.Core.Models.Bezier;
+public class SimplifyPath
+{
+    private static double PointLineDistance(Point2<double> point, Point2<double> lineStart, Point2<double> lineEnd)
+    {
+        var lineDirection = new Point2<double>(lineEnd.X - lineStart.X, lineEnd.Y - lineStart.Y);
+        var pointDirection = new Point2<double>(point.X - lineStart.X, point.Y - lineStart.Y);
+
+        var projectedLength = pointDirection.X * lineDirection.X + pointDirection.Y * lineDirection.Y;
+        var projectedPoint = new Point2<double>(lineStart.X + (lineDirection.X * projectedLength), lineStart.Y + (lineDirection.Y * projectedLength));
+
+        return Math.Sqrt((point.X - projectedPoint.X) * (point.X - projectedPoint.X) + (point.Y - projectedPoint.Y) * (point.Y - projectedPoint.Y));
+    }
+
+    private static void DouglasPeucker(List<Point2<double>> points, int startIndex, int endIndex, double epsilon, List<Point2<double>> simplifiedPoints)
+    {
+        double maxDistance = 0;
+        var maxIndex = 0;
+
+        for (var i = startIndex + 1; i < endIndex; i++)
+        {
+            var distance = PointLineDistance(points[i], points[startIndex], points[endIndex]);
+
+            if (distance > maxDistance)
+            {
+                maxDistance = distance;
+                maxIndex = i;
+            }
+        }
+
+        if (maxDistance > epsilon)
+        {
+            DouglasPeucker(points, startIndex, maxIndex, epsilon, simplifiedPoints);
+            DouglasPeucker(points, maxIndex, endIndex, epsilon, simplifiedPoints);
+        }
+        else
+        {
+            simplifiedPoints.Add(points[startIndex]);
+        }
+    }
+
+    public static List<Point2<double>> Simplify(List<Point2<double>> points, double approximationAccuracy)
+    {
+        if (points.Count < 3)
+        {
+            return points;
+        }
+
+        var simplifiedPoints = new List<Point2<double>>();
+        DouglasPeucker(points, 0, points.Count - 1, approximationAccuracy, simplifiedPoints);
+        simplifiedPoints.Add(points[^1]);
+
+        return simplifiedPoints;
+    }
+}
