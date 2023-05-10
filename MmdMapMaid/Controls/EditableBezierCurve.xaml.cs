f44b4774499a8f027cd3a4dc0e2f007eefab7729
Inwerwm <saclainvervm@gmail.com>
2023-05-10 20:51:08 +0900

    原点を四つ角の内から選べるようにした

--- MmdMapMaid/Controls/EditableBezierCurve.xaml.cs
+++ MmdMapMaid/Controls/EditableBezierCurve.xaml.cs
@@ -8,6 +8,14 @@ using Windows.Foundation;
 namespace MmdMapMaid.Controls;
 public sealed partial class EditableBezierCurve : UserControl
 {
+    public enum OriginPosition
+    {
+        TopLeft,
+        BottomLeft,
+        BottomRight,
+        TopRight
+    }
+
     public static readonly DependencyProperty EarlierControlPointProperty =
         DependencyProperty.Register(
             nameof(EarlierControlPoint),
@@ -29,6 +37,13 @@ public sealed partial class EditableBezierCurve : UserControl
             typeof(EditableBezierCurve),
             new PropertyMetadata(default(double), SizePropertyChanged));
 
+    public static readonly DependencyProperty OriginPositionProperty =
+        DependencyProperty.Register(
+            nameof(Origin),
+            typeof(OriginPosition),
+            typeof(EditableBezierCurve),
+            new PropertyMetadata(default(OriginPosition), OriginPositionPropertyChanged));
+
     public Point EarlierControlPoint
     {
         get => (Point)GetValue(EarlierControlPointProperty);
@@ -47,9 +62,16 @@ public sealed partial class EditableBezierCurve : UserControl
         set => SetValue(SizeProperty, value);
     }
 
+    public OriginPosition Origin
+    {
+        get => (OriginPosition)GetValue(OriginPositionProperty);
+        set => SetValue(OriginPositionProperty, value);
+    }
+
     private Point StartPoint
     {
         get;
+        set;
     }
 
     private Point EndPoint
@@ -64,8 +86,8 @@ public sealed partial class EditableBezierCurve : UserControl
 
         Size = 1000;
 
-        StartPoint = new Point(0, 0);
-        EndPoint = new Point(Size, Size);
+        StartPoint = GetStartPoint();
+        EndPoint = GetEndPoint();
 
         // ‰Šú’l‚ð 0.0 ` 1.0 ‚Ì”ÍˆÍ‚ÉÝ’è
         EarlierControlPoint = new Point(0.25, 0.25);
@@ -84,6 +106,30 @@ public sealed partial class EditableBezierCurve : UserControl
 
         AddDragHandlers(EarlierControlPointHandle, p => EarlierControlPoint = CalculateNormalizedCoordinates(p));
         AddDragHandlers(LaterControlPointHandle, p => LaterControlPoint = CalculateNormalizedCoordinates(p));
+    }
+
+    private Point GetStartPoint()
+    {
+        return Origin switch
+        {
+            OriginPosition.TopLeft => new Point(0, 0),
+            OriginPosition.BottomLeft => new Point(0, Size),
+            OriginPosition.BottomRight => new Point(Size, Size),
+            OriginPosition.TopRight => new Point(Size, 0),
+            _ => new Point(0, 0),
+        };
+    }
+
+    private Point GetEndPoint()
+    {
+        return Origin switch
+        {
+            OriginPosition.TopLeft => new Point(Size, Size),
+            OriginPosition.BottomLeft => new Point(Size, 0),
+            OriginPosition.BottomRight => new Point(0, 0),
+            OriginPosition.TopRight => new Point(0, Size),
+            _ => new Point(Size, Size),
+        };
     }
 
     private static void EarlierControlPointPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
@@ -120,7 +166,27 @@ public sealed partial class EditableBezierCurve : UserControl
         instance.CurveCanvas.Width = newSize;
         instance.CurveCanvas.Height = newSize;
 
-        instance.EndPoint = new Point(newSize, newSize);
+        instance.EndPoint = instance.GetEndPoint();
+
+        instance.DrawCubicBezier(
+            instance.StartPoint,
+            instance.CalculateCanvasCoordinates(instance.EarlierControlPoint),
+            instance.CalculateCanvasCoordinates(instance.LaterControlPoint),
+            instance.EndPoint
+        );
+
+        UpdateLine(instance.EarlierLine, instance.StartPoint, instance.CalculateCanvasCoordinates(instance.EarlierControlPoint));
+        UpdateLine(instance.LaterLine, instance.CalculateCanvasCoordinates(instance.LaterControlPoint), instance.EndPoint);
+
+        SetHandlePosition(instance.EarlierControlPointHandle, instance.CalculateCanvasCoordinates(instance.EarlierControlPoint));
+        SetHandlePosition(instance.LaterControlPointHandle, instance.CalculateCanvasCoordinates(instance.LaterControlPoint));
+    }
+
+    private static void OriginPositionPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+    {
+        var instance = ((EditableBezierCurve)dependencyObject);
+        instance.StartPoint = instance.GetStartPoint();
+        instance.EndPoint = instance.GetEndPoint();
 
         instance.DrawCubicBezier(
             instance.StartPoint,
@@ -152,13 +218,48 @@ public sealed partial class EditableBezierCurve : UserControl
 
     private Point CalculateNormalizedCoordinates(Point canvasPoint)
     {
-        return new Point(canvasPoint.X / Size, canvasPoint.Y / Size);
+        var x = canvasPoint.X / Size;
+        double y = canvasPoint.Y / Size;
+
+        switch (Origin)
+        {
+            case OriginPosition.BottomLeft:
+                y = 1 - y;
+                break;
+            case OriginPosition.TopRight:
+                x = 1 - x;
+                break;
+            case OriginPosition.BottomRight:
+                x = 1 - x;
+                y = 1 - y;
+                break;
+        }
+
+        return new Point(x, y);
     }
 
     private Point CalculateCanvasCoordinates(Point normalizedPoint)
     {
-        return new Point(normalizedPoint.X * Size, normalizedPoint.Y * Size);
+        var x = normalizedPoint.X * Size;
+        var y = normalizedPoint.Y * Size;
+
+        switch (Origin)
+        {
+            case OriginPosition.BottomLeft:
+                y = Size - y;
+                break;
+            case OriginPosition.TopRight:
+                x = Size - x;
+                break;
+            case OriginPosition.BottomRight:
+                x = Size - x;
+                y = Size - y;
+                break;
+        }
+
+        return new Point(x, y);
     }
+
 
     private void DrawCubicBezier(Point p0, Point p1, Point p2, Point p3)
     {
--- MmdMapMaid/ViewModels/MorphInterpolationViewModel.cs
+++ MmdMapMaid/ViewModels/MorphInterpolationViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Windows.Foundation;
 
 namespace MmdMapMaid.ViewModels;
 
-public class MorphInterpolationViewModel : ObservableRecipient
+public partial class MorphInterpolationViewModel : ObservableRecipient
 {
+    [ObservableProperty]
+    private Point _earlierPoint;
+
+    [ObservableProperty]
+    private Point _laterPoint;
+
     public MorphInterpolationViewModel()
     {
+        EarlierPoint = new(0.25, 0.25);
+        LaterPoint = new(0.75, 0.75);
     }
 }
