f96a1527ebff98816f335d8f4e9b41d11008f10a
Inwerwm <saclainvervm@gmail.com>
2023-06-03 17:25:18 +0900

    記述を簡略化

--- MmdMapMaid.Core/Helpers/Json.cs
+++ MmdMapMaid.Core/Helpers/Json.cs
@@ -4,23 +4,15 @@ namespace MmdMapMaid.Core.Helpers;
 
 public static class Json
 {
-    public static async Task<T?> ToObjectAsync<T>(string value, JsonSerializerOptions? options = null)
-    {
-        return await Task.Run(() => JsonSerializer.Deserialize<T>(value, options));
-    }
+    public static async Task<T?> ToObjectAsync<T>(string value, JsonSerializerOptions? options = null) =>
+        await Task.Run(() => JsonSerializer.Deserialize<T>(value, options));
 
-    public static T? ToObject<T>(string value, JsonSerializerOptions? options = null)
-    {
-        return JsonSerializer.Deserialize<T>(value, options);
-    }
+    public static T? ToObject<T>(string value, JsonSerializerOptions? options = null) =>
+        JsonSerializer.Deserialize<T>(value, options);
 
-    public static async Task<string> StringifyAsync(object? value, JsonSerializerOptions? options = null)
-    {
-        return await Task.Run(() => JsonSerializer.Serialize(value, options));
-    }
+    public static async Task<string> StringifyAsync(object? value, JsonSerializerOptions? options = null) =>
+        await Task.Run(() => JsonSerializer.Serialize(value, options));
 
-    public static string Stringify(object? value, JsonSerializerOptions? options = null)
-    {
-        return JsonSerializer.Serialize(value, options);
-    }
+    public static string Stringify(object? value, JsonSerializerOptions? options = null) =>
+        JsonSerializer.Serialize(value, options);
 }
